c01a933127912fdf0d11778f73bb191fd155d50c
Abdon Morales <abdonm@cs.utexas.edu>
2025-04-18 09:09:56 -0500

    Implemented more functionality to the GUI
    Added more functionality to the GUI, including the implementation of
    functional buttons and redirections along with using nested classes for
    creating windows to display core information. More functionality is on
    the way, but for now we have now implement the expected wireframe of the
    project.
    
    Signed-off-by: Abdon Morales <abdonm@cs.utexas.edu>

--- src/LonghornNetworkGUI.java
+++ src/LonghornNetworkGUI.java
@@ -1,50 +1,136 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LonghornNetworkGUI {
-    //private static JProgressBar progressBar;
     private JFrame frame;
 
     public LonghornNetworkGUI() {
-        // Force the Look and Feel to use Cross Platform for compat.
-        try {
-            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        setLookAndFeel();
 
         // Build the interface.
         frame = new JFrame("Longhorn Network");
         JPanel panel = new JPanel();
-        //frame.setSize(400, 600);
+        frame.setSize(800, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.getContentPane().setLayout(new BorderLayout(10, 10));
-        //((JPanel) frame.getContentPane()).setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-
+        JPanel centerPanel = new JPanel();
         JLabel label = new JLabel("Welcome to Longhorn Network");
         label.setFont(label.getFont().deriveFont(20f));
-        frame.add(label, BorderLayout.NORTH);
-
-        JPanel centerPanel = new JPanel();
-        centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
-        //centerPanel.add(Box.createRigidArea(new Dimension(0, 5)));
+        JComponent imageComponent = new JLabel(new ImageIcon("Longhorn.png"));
+        // TODO: Fix the components layout issue with the image and label.
+        panel.add(label, BorderLayout.CENTER);
+        panel.add(imageComponent);
+        frame.add(panel, BorderLayout.NORTH);
 
         JTextField searchField = new JTextField("Enter a name here and click Search");
-        centerPanel.add(searchField);
+        searchField.setFont(searchField.getFont().deriveFont(14f));
+        centerPanel.add(searchField, BorderLayout.SOUTH);
 
-        //centerPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+        JPanel buttonPanel = new JPanel(new FlowLayout());
         JButton searchButton = new JButton("Search");
         searchButton.setContentAreaFilled(true);
         searchButton.setBackground(new Color(191, 87, 0));
-        centerPanel.add(searchButton);
+        searchButton.addActionListener(search(searchField));
+        buttonPanel.add(searchButton);
+
+        JButton dispStudentGraphButton = new JButton("Display Student Graph");
+        dispStudentGraphButton.setContentAreaFilled(true);
+        dispStudentGraphButton.setBackground(new Color(191, 87, 0));
+        dispStudentGraphButton.addActionListener(studentGraph());
+        buttonPanel.add(dispStudentGraphButton);
+
         frame.add(centerPanel, BorderLayout.CENTER);
+        frame.add(buttonPanel, BorderLayout.SOUTH);
 
         // Center the window upon execution
         frame.setLocationRelativeTo(null);
-        frame.pack();
         frame.setVisible(true);
     }
+
+    private ActionListener studentGraph() {
+        return new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                JFrame window = new JFrame("Student Graph");
+                window.setSize(800, 600);
+                window.setLocationRelativeTo(null);
+                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                JPanel panel = new JPanel();
+
+                window.setVisible(true);
+            }
+        };
+    }
+
+    /**
+     *
+     */
+    private static void setLookAndFeel() {
+        // We are going Old-Skool here, back to the good ol' Sun days.
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+        } catch (Exception e) {
+            System.err.println("Failed to set look and feel: " +
+                    "com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+        }
+    }
+
+    /**
+     * @param searchField
+     * @return
+     */
+    private ActionListener search(JTextField searchField) {
+
+        return new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+                new UserInfoWindow(searchField.getText());
+            }
+        };
+    }
+
+    public class UserInfoWindow {
+
+        public UserInfoWindow(String name) {
+            JFrame window = new JFrame();
+            window.setTitle("Longhorn Network -- " + name.trim());
+            window.setSize(800, 600);
+            window.setLocationRelativeTo(null);
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JTabbedPane tabbedPane = new JTabbedPane();
+
+            //
+            JPanel StudentInformationPanel = new JPanel();
+            tabbedPane.addTab("Student Information", StudentInformationPanel);
+
+            //
+            JPanel ReferralPanel = new JPanel();
+            tabbedPane.addTab("Referrals", ReferralPanel);
+
+            //
+            JPanel ChatPanel = new JPanel();
+            tabbedPane.addTab("Chats", ChatPanel);
+
+            //
+            JPanel FriendsPanel = new JPanel();
+            tabbedPane.addTab("Friends", FriendsPanel);
+
+            JButton returnButton = new JButton("Return to Main Menu");
+            returnButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    window.dispose();
+                    frame.setVisible(true);
+                }
+            });
+
+            window.add(tabbedPane);
+            window.setVisible(true);
+        }
+    }
+
 
     public static void main(String[] args) {
         new LonghornNetworkGUI();

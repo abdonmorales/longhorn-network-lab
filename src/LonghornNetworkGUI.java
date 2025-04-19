e6467abfdf1c8cf58b693c400937a06bd6351db3
Abdon Morales <abdonmorales@my.utexas.edu>
2025-04-18 23:19:52 -0500

    JavaDoc Refresh
    Signed-off-by: Abdon Morales <abdonmorales@my.utexas.edu>

--- src/LonghornNetworkGUI.java
+++ src/LonghornNetworkGUI.java
@@ -11,7 +11,8 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * This class implements the front-end or the graphical user interface for
+ * This class implements the front-end or the graphical user interface for 
+ * the Longhorn Network application [LN Lab for ECE 422C].
  *
  * @author Abdon Morales, am226923,
  * <a href="mailto:abdonmorales@my.utexas.edu">abdonmorales@my.utexas.edu</a>
@@ -29,7 +30,7 @@ public class LonghornNetworkGUI {
 
     /// The private and public methods fo LonghornNetworkGUI begin here.
     /**
-     *
+     * This constructor creates the main GUI for the Longhorn Network application.
      */
     public LonghornNetworkGUI() {
         setLookAndFeel();
@@ -64,12 +65,15 @@ public class LonghornNetworkGUI {
 
         // Build the bottom panel to display the buttons for search and display student graph
         JPanel buttonPanel = new JPanel();
+
+        // Build the button to search for a student
         JButton searchButton = new JButton("Search");
         searchButton.setContentAreaFilled(true);
         searchButton.setBackground(new Color(191, 87, 0));
         searchButton.addActionListener(search(searchField));
         buttonPanel.add(searchButton);
 
+        // Build the button to display the student graph
         JButton dispStudentGraphButton = new JButton("Display Student Graph");
         dispStudentGraphButton.setContentAreaFilled(true);
         dispStudentGraphButton.setBackground(new Color(191, 87, 0));
@@ -86,8 +90,14 @@ public class LonghornNetworkGUI {
     }
 
     /**
+     * This method creates a JTextField for the search bar. It sets the placeholder text for 
+     * the search bar so users know what to enter. It also sets the font and color of the 
+     * text field.
      *
-     * @return
+     * @return searchField, the JTextField for the search bar
+     * @see JTextField
+     * @see FocusAdapter
+     * @see FocusEvent
      */
     private JTextField getSearchField() {
         JTextField searchField = new JTextField();
@@ -117,21 +127,30 @@ public class LonghornNetworkGUI {
     }
 
     /**
+     * This helper method builds the menu bar for the GUI. It contains a File menu with options 
+     * to open a student data file and exit the application. It also includes a Help menu 
+     * with an "About" item. The menu bar is added to the main frame of the application.
+     * @see JMenu
      *
-     * @param menuBar
-     * @param frame
+     * @param menuBar, the menu bar to be added to the frame
+     * @see JMenuBar
+     * @param frame, the main frame of the application
+     * @see JFrame
      */
     private void buildMenuBar(JMenuBar menuBar, JFrame frame) {
         JMenu fileMenu = new JMenu("File");
         JMenuItem openItem = new JMenuItem("Open Student Data File");
         openItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                // Create a file chooser to select the student data file
                 JFileChooser fileChooser = new JFileChooser();
                 fileChooser.setDialogTitle("Opening Student Data File...");
                 fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
                 fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 
                 int returnVal = fileChooser.showOpenDialog(frame);
+                
+                // Check if the user selected a file
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
                     file = fileChooser.getSelectedFile();
                     String path = file.getAbsolutePath();
@@ -155,22 +174,31 @@ public class LonghornNetworkGUI {
                 System.exit(0);
             }
         });
+        // Add the menu items to the file menu
         fileMenu.add(openItem);
         fileMenu.addSeparator();
         fileMenu.add(exitItem);
         menuBar.add(fileMenu);
 
+        // Add the Help menu to the menu bar
         JMenu helpMenu = getHelpMenu(frame);
         menuBar.add(helpMenu);
 
+        // Set the background color of the menu bar
         menuBar.setBackground(new Color(191, 87, 0));
         frame.setJMenuBar(menuBar);
     }
 
     /**
+     * This method creates the Help menu for the GUI. It contains an "About" item that displays 
+     * the application version, purpose, and copyright information.
      *
-     * @param frame
-     * @return
+     * @param frame, the main frame of the application
+     * @return helpMenu, the Help menu with the "About" item
+     * @see JMenu
+     * @see JMenuItem
+     * @see ActionListener
+     * @see JOptionPane
      */
     private static JMenu getHelpMenu(JFrame frame) {
         JMenu helpMenu = new JMenu("Help");
@@ -190,25 +218,34 @@ public class LonghornNetworkGUI {
     }
 
     /**
-     *
-     * @return
+     * This method implements the ActionListener for the display student graph button.
+     * @see ActionListener
+     * @return studentGraph, which is an ActionListener that implements the functionality to display the student graph.
+     * @see GraphPanel
      */
     private ActionListener studentGraph() {
         return new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
+
+                // Create the window
                 JFrame window = new JFrame("Student Graph");
                 window.getContentPane().setLayout(new BorderLayout());
                 window.setSize(WINDOW_WIDTH, WINDOW_HEIGTH);
                 window.setLocationRelativeTo(null);
                 window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                // Create the menu bar
                 JMenuBar menuBar = new JMenuBar();
                 menuBar.setBackground(new Color(191, 87, 0));
+
+                // Create the menu bar with a return button
                 JButton returnButton = new JButton("Return to Main Menu");
                 returnButton.addActionListener(goHome(window));
                 menuBar.add(returnButton);
-
                 window.setJMenuBar(menuBar);
+
+                // Create the graph panel and add it to the window
                 if (studentData != null) {
                      GraphPanel graphPanel = new GraphPanel();
                      window.getContentPane().add(graphPanel, BorderLayout.CENTER);
@@ -236,9 +273,12 @@ public class LonghornNetworkGUI {
     }
 
     /**
+     * This method implements the ActionListener for the return button in the UserInfoWindow and elsewhere.
      *
-     * @param window
-     * @return
+     * @param window, the window to be closed
+     * @see JFrame
+     * @see ActionListener
+     * @return goHome, which is an ActionListener that implements the functionality to return to the main menu.
      */
     private ActionListener goHome(JFrame window) {
         return new ActionListener() {
@@ -251,9 +291,11 @@ public class LonghornNetworkGUI {
 
     /**
      * This method implements the search functionality for the search button, or
-     * whichever component adds it to its ActionListener
+     * whichever component adds it to its ActionListener.
      * @param searchField, This passes through the text-field to grab the string that the user typed.
-     * @return search
+     * @return search, which is an ActionListener that implements the search functionality.
+     * @see ActionListener
+     * @see JTextField
      */
     private ActionListener search(JTextField searchField) {
         return new ActionListener() {
@@ -261,12 +303,17 @@ public class LonghornNetworkGUI {
                 frame.dispose();
                 try {
                     boolean found = false;
+
+                    // Iterate through the student data to find the student
                     for (UniversityStudent student : studentData) {
+
+                        // Check if the student name matches the search field
                         if (student.name.equalsIgnoreCase(searchField.getText())) {
                             found = true;
                             new UserInfoWindow(student);
                         }
                     }
+                    // If the student is not found, show a warning message
                     if (!found) {
                         JOptionPane.showMessageDialog(frame,
                                 "Student is not found, please try again.",
@@ -289,6 +336,13 @@ public class LonghornNetworkGUI {
     /// Private and public methods of LonghornNetworkGUI end here. ///
 
     /**
+     * This class implements the GraphPanel that 
+     * displays the graph of students in a circular layout using 
+     * a @see {@link java.util.HashMap} and ArrayLists to provide the maximum amount of 
+     * efficency and quick access times.
+     * 
+     * @author Abdon Morales, am226923, 
+     * <a href="mailto:abdonmorales@my.utexas.edu">abdonmorales@my.utexas.edu</a>
      *
      */
     private class GraphPanel extends JPanel {
@@ -298,7 +352,10 @@ public class LonghornNetworkGUI {
         private final int NODE_SIZE = 40;
 
         /**
-         *
+         * This constructor creates a new GraphPanel object. It sets the preferred
+         * size of the panel to accommodate the graph. The size is based on the
+         * radius of the circle and the size of the nodes. The panel is used to
+         * display the graph in a circular layout.
          */
         public GraphPanel() {
             setPreferredSize(new Dimension(2*RADIUS + NODE_SIZE + 20,
@@ -306,7 +363,10 @@ public class LonghornNetworkGUI {
         }
 
         /**
-         *
+         * This method computes the layout of the graph in a circular manner.
+         * It calculates the coordinates of each node in the graph and stores
+         * them in a map. The nodes are arranged in a circle with a specified
+         * radius. The center of the circle is at the center of the panel.
          */
         private void computerCircLayout() {
             java.util.List<UniversityStudent> nodes = new ArrayList<>(graph.getAllNodes());
@@ -314,7 +374,8 @@ public class LonghornNetworkGUI {
 
             int centerX = getWidth() / 2;
             int centerY = getHeight() / 2;
-
+            
+            // Calculate the coordinates of each node in a circular layout
             for (int i = 0; i < n; i++) {
                 UniversityStudent node = nodes.get(i);
 
@@ -327,8 +388,13 @@ public class LonghornNetworkGUI {
         }
 
         /**
-         *
-         * @param g the <code>Graphics</code> object to protect
+         * This method is called to paint the component. It draws the graph
+         * using the coordinates calculated in the computerCircLayout method.
+         * It uses the Graphics2D object to draw the edges and nodes of the graph.
+         * 
+         * @param g the Graphics object to paint on
+         * @see Graphics
+         * @see Graphics2D
          */
         @Override
         protected void paintComponent(Graphics g) {
@@ -337,11 +403,11 @@ public class LonghornNetworkGUI {
             computerCircLayout();
 
             g2d.setColor(Color.GRAY);
-            //
+            // Get the coordinates of the nodes
             for (UniversityStudent node : graph.getAllNodes()) {
                 Point p1 = coord.get(node);
 
-                //
+                // Draw the edges of the graph
                 for (StudentGraph.Edge edge : graph.getNeighbors(node)) {
                     UniversityStudent vertex = edge.neighbor;
                     Point p2 = coord.get(vertex);
@@ -350,7 +416,7 @@ public class LonghornNetworkGUI {
                 }
             }
 
-            //
+            // Draw the nodes of the graph
             for (UniversityStudent node : graph.getAllNodes()) {
                 Point p1 = coord.get(node);
                 g2d.setColor(Color.BLACK);
@@ -366,22 +432,29 @@ public class LonghornNetworkGUI {
     }
 
     /**
-     *
+     * This class implements the user information window that displays the relevant information of a student. As mentioned in its respective constructor comment. 
+     * @see LonghornNetworkGUI.UserInfoWindow
+     * @author Abdon Morales, am226923, 
+     * <a href="mailto:abdonmorales@my.utexas.edu">abdonmorales@my.utexas.edu</a>
      */
     private class UserInfoWindow {
         Color panelColor = Color.WHITE;
 
         /**
-         *
+         * This constructor creates a new window to display the relevant information of 
+         * the student. Such as, core data, friends, referrals, and chats.
+         * 
          * @param student
          */
         public UserInfoWindow(UniversityStudent student) {
+            // Create the window
             JFrame window = new JFrame();
             window.setTitle("Longhorn Network -- " + student.name.trim());
             window.setSize(WINDOW_WIDTH, WINDOW_HEIGTH);
             window.setLocationRelativeTo(null);
             window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+            // Create the tabbed pane to hold the different pages (panels)
             JTabbedPane tabbedPane = new JTabbedPane();
 
             // Create the student's information page
@@ -412,11 +485,12 @@ public class LonghornNetworkGUI {
             tabbedPane.setBackground(new Color(191, 87, 0));
             window.add(tabbedPane);
 
-            //
+            // Create the bottom panel with the exit button
             JPanel buttonPanel = new JPanel();
             JButton exitButton = new JButton("Return Home");
             exitButton.addActionListener(goHome(window));
 
+            // Set the button properties
             buttonPanel.setBackground(new Color(191, 87, 0));
             buttonPanel.add(exitButton);
             window.add(buttonPanel, BorderLayout.SOUTH);
@@ -425,9 +499,12 @@ public class LonghornNetworkGUI {
         }
 
         /**
+         * This method displays the information of the student in the text area.
          *
-         * @param studentInfoTextArea
-         * @param student
+         * @param studentInfoTextArea, the text area to display the information
+         * @param student, the student object to that contains the information
+         * @see UniversityStudent
+         * @see JTextArea
          */
         private static void displayStudentInfo (JTextArea studentInfoTextArea,
                                                 UniversityStudent student) {
@@ -442,8 +519,9 @@ public class LonghornNetworkGUI {
     }
 
     /**
-     *
-     * @param args
+     * This is the main method that creates an instance of the LonghornNetworkGUI class.
+     * @param args, the command line arguments
+     * @see LonghornNetworkGUI
      */
     public static void main(String[] args) {
         new LonghornNetworkGUI();
